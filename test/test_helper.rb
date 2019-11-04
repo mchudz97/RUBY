d0bcfd02d9dc4d9a34584b7a8a3cd7cb2a5783e2
mchudz97 <mchudz97@gmail.com>
2019-11-04 19:59:10 +0100

    covered?

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,1 @@
 require 'simplecov'
-#SimpleCov.start 'rails'
-
-#SimpleCov.start  do
-#  add_filter '/bin/'
-#  add_filter '/db/'
-#  add_filter '/test/' # for minitest
-#  add_filter '/spec/' # for rspec
-
-#end
-
